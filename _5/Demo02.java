550b636a2e84016048db9fff99ae432bb209dc47
minatoyukina <1096445518@qq.com>
2020-03-04 23:30:23 +0800

    leetcode

---
+++ _5/Demo02.java
@@ -0,0 +1,48 @@
+package leetcode._5;
+
+import org.junit.Test;
+
+public class Demo02 {
+
+    @Test
+    public void test() {
+        System.out.println(longestPalindrome("rgczcpratwyqxaszbuwwcadruayhasynuxnakpmsyhxzlnxmdtsqqlm" +
+                "wnbxvmgvllafrpmlfuqpbhjddmhmbcgmlyeypkfpreddyencsdmgxysctpubvgeedhurvizgqxclhpfrvxggrowaynrtu" +
+                "wvvvwnqlowdihtrdzjffrgoeqivnprdnpvfjuhycpfydjcpfcnkpyujljiesmuxhtizzvwhvpqylvcirwqsmpptyhcqybstsf" +
+                "gjadicwzycswwmpluvzqdvnhkcofptqrzgjqtbvbdxylrylinspncrkxclykccbwridpqckstxdjawvziucrswpsfmisqio" +
+                "zworibeycuarcidbljslwbalcemgymnsxfziattdylrulwrybzztoxhevsdnvvljfzzrgcmagshucoalfiuapgzpqgjjgqsmc" +
+                "vtdsvehewrvtkeqwgmatqdpwlayjcxcavjmgpdyklrjcqvxjqbjucfubgmgpkfdxznkhcejscymuildfnuxwmuklntnyycd" +
+                "cscioimenaeohgpbcpogyifcsatfxeslstkjclauqmywacizyapxlgtcchlxkvygzeucwalhvhbwkvbceqajstxzzppcxoa" +
+                "nhyfkgwaelsfdeeviqogjpresnoacegfeejyychabkhszcokdxpaqrprwfdahjqkfptwpeykgumyemgkccynxuvbdpjlrbg" +
+                "qtcqulxodurugofuwzudnhgxdrbbxtrvdnlodyhsifvyspejenpdckevzqrexplpcqtwtxlimfrsjumiygqeemhihcxyngs" +
+                "emcolrnlyhqlbqbcestadoxtrdvcgucntjnfavylip"));
+        System.out.println(longestPalindrome("abc"));
+    }
+
+    //中心扩展
+    private String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) return "";
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i); //从一个字符扩展
+            int len2 = expandAroundCenter(s, i, i + 1); //从两个字符之间扩展
+            int len = Math.max(len1, len2);
+            //根据 i 和 len 求得字符串的相应下标
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private int expandAroundCenter(String s, int left, int right) {
+        int L = left, R = right;
+        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+            L--;
+            R++;
+        }
+        return R - L - 1;
+    }
+
+}
---
+++ _6/Demo01.java
@@ -0,0 +1,44 @@
+package leetcode._6;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class Demo01 {
+
+    @Test
+    public void test() {
+        System.out.println(convert("ABC", 2));
+        System.out.println(convert("LEETCODEISHIRINGS", 3));
+    }
+
+    private String convert(String s, int numRows) {
+        int peer = (numRows * 2 - 2);
+        if (peer <= 0) {
+            return s;
+        }
+        int round = s.length() / peer + 1;
+        int numCols = (numRows - 1) * round - 1;
+        StringBuilder answer = new StringBuilder();
+        String[][] chars = new String[numRows][numCols];
+        for (int i = 0; i < s.length(); i++) {
+            int x = i / peer;
+            int y = i % peer;
+            if (y < numRows) {
+                chars[y][x * (numRows - 1)] = s.charAt(i) + "";
+            } else {
+                chars[numRows - y % numRows - 2][x * (numRows - 1) + y % numRows + 1] = s.charAt(i) + "";
+            }
+            System.out.println(Arrays.deepToString(chars).replaceAll("null", " "));
+        }
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                String s1 = chars[i][j];
+                if (s1 != null) {
+                    answer.append(s1);
+                }
+            }
+        }
+        return answer.toString();
+    }
+}
