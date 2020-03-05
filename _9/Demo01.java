28c570ba38135e98f65401bd2dd0ff412786e114
minatoyukina <1096445518@qq.com>
2020-03-05 20:17:50 +0800

    leetcode

---
+++ _9/Demo01.java
@@ -0,0 +1,16 @@
+package leetcode._9;
+
+import org.junit.Test;
+
+public class Demo01 {
+
+    @Test
+    public void test() {
+        System.out.println(isPalindrome(123));
+        System.out.println(isPalindrome(121));
+    }
+
+    private boolean isPalindrome(int x) {
+        return new StringBuilder(x + "").reverse().toString().equals(x + "");
+    }
+}
