e6a9d44f27ceda207c85f0740110f2de955cf4cf
minatoyukina <1096445518@qq.com>
2020-03-21 11:07:47 +0800

    leetcode

--- _101/Demo01.java
+++ _101/Demo01.java
@@ -26,6 +26,9 @@ public class Demo01 {
     }
 
     private boolean isSymmetric(TreeNode left, TreeNode right) {
+        if ((left == null && right != null) || (left != null && right == null)) {
+            return false;
+        }
         if (left != null) {
             if (left.val != right.val) {
                 return false;
