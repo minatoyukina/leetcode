c72b3685ce4bfdfef0747b12e0c54a8652389755
minatoyukina <1096445518@qq.com>
2020-03-26 22:38:03 +0800

    leetcode

--- _171/Demo01.java
+++ _171/Demo01.java
@@ -7,9 +7,8 @@ public class Demo01 {
     @Test
     public void test() {
         System.out.println(titleToNumber("AB"));
-        System.out.println(titleToNumber("J"));
-        System.out.println(Math.log(4));
-        System.out.println(Math.log1p(4));
+        System.out.println(titleToNumber("AZZ"));
+        System.out.println(convertToTitle(titleToNumber("AZZS")));
     }
 
     private int titleToNumber(String s) {
@@ -18,5 +17,14 @@ public class Demo01 {
             sum += Math.pow(26, s.length() - i - 1) * ((byte) s.charAt(i) - 64);
         }
         return sum;
+    }
+
+    private String convertToTitle(int n) {
+        StringBuilder sb = new StringBuilder();
+        while (n > 0) {
+            sb.insert(0, (char) ((n % 26 == 0 ? 26 : n % 26) + 64));
+            n = n % 26 == 0 ? n / 26 - 1 : n / 26;
+        }
+        return sb.toString();
     }
 }
---
+++ _206/Demo01.java
@@ -0,0 +1,30 @@
+package leetcode._206;
+
+import leetcode._2.ListNode;
+import org.junit.Test;
+
+import java.util.Stack;
+
+public class Demo01 {
+
+    @Test
+    public void test() {
+        System.out.println(reverseList(new ListNode(1, new ListNode(2, new ListNode(3)))));
+    }
+
+    private ListNode reverseList(ListNode head) {
+        Stack<ListNode> stack = new Stack<>();
+        while (head != null) {
+            stack.push(new ListNode(head.val));
+            head = head.next;
+        }
+        System.out.println(stack);
+        ListNode node = stack.pop();
+        ListNode tmp = node;
+        while (!stack.isEmpty()) {
+            tmp.next = stack.pop();
+            tmp = tmp.next;
+        }
+        return node;
+    }
+}
