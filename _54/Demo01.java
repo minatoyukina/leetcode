8495cb28642cd69ddb069ef504856102be72f097
minatoyukina <1096445518@qq.com>
2020-04-03 15:32:28 +0800

    leetcode

---
+++ _54/Demo01.java
@@ -0,0 +1,59 @@
+package leetcode._54;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Demo01 {
+
+    @Test
+    public void test() {
+        System.out.println(spiralOrder(new int[][]{
+                new int[]{1, 2, 3, 4},
+                new int[]{5, 6, 7, 8},
+                new int[]{9, 10, 11, 12},
+        }));
+        System.out.println(spiralOrder(new int[][]{}));
+    }
+
+    private List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> list = new ArrayList<>();
+        if (Arrays.deepEquals(matrix, new int[][]{})) return list;
+        int direct = 0, x = 0, y = 0, m = matrix.length, n = matrix[0].length;
+        for (int i = 0; i < m * n; i++) {
+            if (direct % 4 == 0) {
+                if (y == n - direct / 4) {
+                    y--;
+                    x++;
+                    direct++;
+                } else list.add(matrix[x][y++]);
+            }
+            if (direct % 4 == 1) {
+                if (x == m - direct / 4) {
+                    x--;
+                    y--;
+                    direct++;
+                } else list.add(matrix[x++][y]);
+            }
+            if (direct % 4 == 2) {
+                if (y == -1 + direct / 4) {
+                    y++;
+                    x--;
+                    direct++;
+                } else list.add(matrix[x][y--]);
+            }
+            if (direct % 4 == 3) {
+                if (x == direct / 4) {
+                    x++;
+                    y++;
+                    direct++;
+                    i--;
+                } else list.add(matrix[x--][y]);
+            }
+
+        }
+        return list;
+    }
+}
--- _59/Demo01.java
+++ _59/Demo01.java
@@ -2,25 +2,48 @@ package leetcode._59;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.stream.Collectors;
 
 public class Demo01 {
 
     @Test
     public void test() {
-        System.out.println(Arrays.deepToString(generateMatrix(3)));
+        Arrays.stream(generateMatrix(4)).forEach(s -> System.out.println(Arrays.toString(s)));
+        Arrays.stream(generateMatrix(5)).forEach(s -> System.out.println(Arrays.toString(s)));
     }
 
     private int[][] generateMatrix(int n) {
         int[][] matrix = new int[n][n];
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-
+        int direct = 0, x = 0, y = 0;
+        for (int i = 1; i <= n * n; i++) {
+            if (direct % 4 == 0) {
+                if (y == n || matrix[x][y] != 0) {
+                    y--;
+                    x++;
+                    direct++;
+                } else matrix[x][y++] = i;
+            }
+            if (direct % 4 == 1) {
+                if (x == n || matrix[x][y] != 0) {
+                    x--;
+                    y--;
+                    direct++;
+                } else matrix[x++][y] = i;
+            }
+            if (direct % 4 == 2) {
+                if (y == -1 || matrix[x][y] != 0) {
+                    y++;
+                    x--;
+                    direct++;
+                } else matrix[x][y--] = i;
+            }
+            if (direct % 4 == 3) {
+                if (x == -1 || matrix[x][y] != 0) {
+                    x++;
+                    y++;
+                    direct++;
+                    i--;
+                } else matrix[x--][y] = i;
             }
         }
         return matrix;
