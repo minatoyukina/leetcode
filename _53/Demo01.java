38a55da274694f5c49beeba62fd116780cc75068
minatoyukina <1096445518@qq.com>
2020-04-07 23:32:21 +0800

    leetcode

--- _53/Demo01.java
+++ _53/Demo01.java
@@ -10,6 +10,13 @@ public class Demo01 {
     }
 
     private int maxSubArray(int[] nums) {
-        return 0;
+        if (nums.length == 0) return 0;
+        int sum = nums[0];
+        int ans = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            sum = Math.max(sum + nums[i], nums[i]);
+            if (sum > ans) ans = sum;
+        }
+        return ans;
     }
 }
