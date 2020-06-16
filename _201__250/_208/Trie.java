8078bf414a6106123d541d59bfa22702cc57539c
minatoyukina <1096445518@qq.com>
2020-06-16 23:43:36 +0800

    leetcode

--- _201__250/_208/Trie.java
+++ _201__250/_208/Trie.java
@@ -1,45 +1,100 @@
 package leetcode._201__250._208;
 
-import org.junit.Test;
+import java.util.ArrayList;
+import java.util.List;
 
+@SuppressWarnings("all")
 public class Trie {
 
-    private StringBuilder sb;
-
-    @Test
-    public void test() {
+    public static void main(String[] args) {
         Trie trie = new Trie();
-
         trie.insert("apple");
         System.out.println(trie.search("apple"));
         System.out.println(trie.search("app"));
         System.out.println(trie.startsWith("app"));
         trie.insert("app");
         System.out.println(trie.search("app"));
+        trie.insert("bbba");
+        trie.insert("bb");
+        trie.insert("bc");
+        System.out.println(trie.startsWith("b"));
+        System.out.println(trie);
+    }
+
+    @Override
+    public String toString() {
+        return "Trie{" +
+                "word=" + word +
+                ", isEnd=" + isEnd +
+                ", child=" + child +
+                '}';
+    }
+
+    private char word;
+    private boolean isEnd;
+    private List<Trie> child;
+
+    public Trie(char word, boolean isEnd, List<Trie> child) {
+        this.word = word;
+        this.isEnd = isEnd;
+        this.child = child;
     }
 
     public Trie() {
-        sb = new StringBuilder();
+        this(Character.MIN_VALUE, false, new ArrayList<>());
     }
 
-    /**
-     * Inserts a word into the trie.
-     */
     public void insert(String word) {
-        sb.append(word);
+        char[] chars = word.toCharArray();
+        List<Trie> list = this.child;
+        label:
+        for (int i = 0; i < chars.length; i++) {
+            for (Trie trie : list) {
+                if (trie.word == chars[i]) {
+                    if (i == chars.length - 1) trie.isEnd = true;
+                    list = trie.child;
+                    continue label;
+                }
+            }
+            Trie trie = new Trie(chars[i], i == chars.length - 1, new ArrayList<>());
+            list.add(trie);
+            list = trie.child;
+        }
     }
 
-    /**
-     * Returns if the word is in the trie.
-     */
     public boolean search(String word) {
-        return sb.indexOf(word) > -1;
+        char[] chars = word.toCharArray();
+        List<Trie> list = this.child;
+        for (int i = 0; i < chars.length; i++) {
+            int count = 0;
+            for (Trie trie : list) {
+                count++;
+                if (trie.word == chars[i]) {
+                    list = trie.child;
+                    if (i == chars.length - 1 && trie.isEnd) return true;
+                    break;
+                }
+                if (count == list.size()) return false;
+            }
+        }
+        return false;
     }
 
-    /**
-     * Returns if there is any word in the trie that starts with the given prefix.
-     */
     public boolean startsWith(String prefix) {
-        return sb.toString().startsWith(prefix);
+        char[] chars = prefix.toCharArray();
+        List<Trie> list = this.child;
+        for (int i = 0; i < chars.length; i++) {
+            int count = 0;
+            for (Trie trie : list) {
+                count++;
+                if (trie.word == chars[i]) {
+                    list = trie.child;
+                    if (i == chars.length - 1) return true;
+                    break;
+                }
+                if (count == list.size()) return false;
+            }
+        }
+        return false;
     }
 }
