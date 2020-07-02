cef811ce45d462d757e4b69a6127bc86ac99ddfb
minatoyukina <1096445518@qq.com>
2020-07-02 22:29:03 +0800

    leetcode

--- _1395/Demo01.java
+++ _1395/Demo01.java
@@ -1,11 +1,6 @@
 package leetcode._1395;
 
-import javafx.util.Pair;
 import org.junit.Test;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Demo01 {
 
@@ -18,30 +13,26 @@ public class Demo01 {
 
 
     private int numTeams(int[] rating) {
-        List<Pair<Boolean, List<Integer>>> list = new CopyOnWriteArrayList<>();
-        for (int i : rating) {
-            List<Integer> l1 = new ArrayList<>();
-            l1.add(i);
-            Pair<Boolean, List<Integer>> p1 = new Pair<>(true, l1);
-            Pair<Boolean, List<Integer>> p2 = new Pair<>(false, new ArrayList<>(l1));
-            list.add(p1);
-            list.add(p2);
-            for (Pair<Boolean, List<Integer>> pair : list) {
-                List<Integer> value = pair.getValue();
-                if (pair.getKey() && value.get(value.size() - 1) > i && value.size() <= 2) {
-                    List<Integer> copy = new ArrayList<>(value);
-                    copy.add(i);
-                    list.add(new Pair<>(true, copy));
-                }
-                if (!pair.getKey() && value.get(value.size() - 1) < i && value.size() <= 2) {
-                    List<Integer> copy = new ArrayList<>(value);
-                    copy.add(i);
-                    list.add(new Pair<>(false, copy));
-                }
+        int ans = 0;
+        for (int i = 0; i < rating.length; i++) {
+            int left = 0, right = 0;
+            for (int x = 0; x < i; x++) {
+                if (rating[x] < rating[i]) left++;
             }
+            for (int x = i + 1; x < rating.length; x++) {
+                if (rating[x] > rating[i]) right++;
+            }
+            ans += left * right;
+            left = 0;
+            right = 0;
+            for (int x = 0; x < i; x++) {
+                if (rating[x] > rating[i]) left++;
+            }
+            for (int x = i + 1; x < rating.length; x++) {
+                if (rating[x] < rating[i]) right++;
+            }
+            ans += left * right;
         }
-        return (int) list.stream()
-                .map(Pair::getValue).filter(x -> x.size() == 3)
-                .count();
+        return ans;
     }
 }
