dc9644ed116ee8e737f8b65d1e9100f688824875
minatoyukina <1096445518@qq.com>
2020-08-02 16:21:43 +0800

    leetcode

---
+++ _501__550/_515/Demo01.java
@@ -0,0 +1,38 @@
+package leetcode._501__550._515;
+
+import leetcode._50__100._100.TreeNode;
+import leetcode.util.Common;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Demo01 {
+
+
+    @Test
+    public void test() {
+        System.out.println(largestValues(Common.arrayToTree(new Integer[]{1, 3, 2, 5, 3, null, 9})));
+
+    }
+
+    private List<Integer> largestValues(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        dfs(root, 0, list);
+        return list;
+    }
+
+    private void dfs(TreeNode root, int level, List<Integer> list) {
+        if (root != null) {
+            if (list.size() > level) {
+                if (list.get(level) < root.val) {
+                    list.remove(level);
+                    list.add(level, root.val);
+                }
+            } else list.add(root.val);
+            dfs(root.left, level + 1, list);
+            dfs(root.right, level + 1, list);
+        }
+    }
+
+}
