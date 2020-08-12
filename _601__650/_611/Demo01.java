d8e30c9ac56378b8dd9adbae14a48e6455742827
minatoyukina <1096445518@qq.com>
2020-08-12 22:47:47 +0800

    leetcode

---
+++ _601__650/_611/Demo01.java
@@ -0,0 +1,31 @@
+package leetcode._601__650._611;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class Demo01 {
+
+
+    @Test
+    public void test() {
+        System.out.println(triangleNumber(new int[]{2, 2, 3, 4}));
+    }
+
+    private int triangleNumber(int[] nums) {
+        Arrays.sort(nums);
+        int ans = 0;
+        for (int i = nums.length - 1; i >= 2; i--) {
+            int lo = 0, hi = i - 1;
+            while (lo < hi) {
+                if (nums[lo] + nums[hi] > nums[i]) {
+                    ans += hi - lo;
+                    hi--;
+                } else lo++;
+            }
+        }
+        return ans;
+    }
+
+
+}
