d3eb9174ffc5656c93b92beef4588d78a6df84a3
minatoyukina <1096445518@qq.com>
2020-08-25 23:08:41 +0800

    leetcode

--- _401__450/_424/Demo01.java
+++ _401__450/_424/Demo01.java
@@ -1,6 +1,5 @@
 package leetcode._401__450._424;
 
-import javafx.util.Pair;
 import org.junit.Test;
 
 import java.util.HashMap;
@@ -11,23 +10,33 @@ public class Demo01 {
 
     @Test
     public void test() {
-        System.out.println(characterReplacement("ABAABA", 2));
+        System.out.println(characterReplacement("ABBB", 2));
+        System.out.println(characterReplacement("ABAAA", 0));
+        System.out.println(characterReplacement("ABAB", 2));
+        System.out.println(characterReplacement("AABABBA", 1));
         System.out.println(characterReplacement("ABAABACA", 3));
     }
 
     private int characterReplacement(String s, int k) {
-        Map<Character, Pair<Integer, Integer>> map = new HashMap<>();
+        Map<Character, Integer> map = new HashMap<>();
         char[] chars = s.toCharArray();
-        for (char c : chars) {
-            if (map.containsKey(c)) {
-                Pair<Integer, Integer> pair = map.get(c);
-                if (pair.getKey() > 0)
-                    map.put(c, new Pair<>(pair.getKey() - 1, pair.getValue() + 1));
+        int ans = 0, l = 0, r = 0, tmp = k;
+        while (r < s.length()) {
+            if (tmp > 0 || (tmp == 0 && chars[l] == chars[r])) {
+                if (chars[l] != chars[r]) tmp--;
+                map.put(chars[r], map.getOrDefault(chars[r], 0) + 1);
+                r++;
+                if (r == s.length()) ans = Math.max(ans, r - l + Math.min(tmp, l));
             } else {
-                map.put(c, new Pair<>(k, 1));
+                ans = Math.max(ans, r - l);
+                if (l + 1 < chars.length) {
+                    map.put(chars[l], map.get(chars[l]) - 1);
+                    l++;
+                    tmp = k - (r - l - map.getOrDefault(chars[l], 0));
+                }
             }
         }
-        return map.values().stream().mapToInt(Pair::getValue).max().orElse(0);
+        return ans;
     }
 
 }
