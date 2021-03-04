372521f47aafb25ba91023f04af7bed478b54ec1
minatoyukina <1096445518@qq.com>
2021-03-04 23:22:22 +0800

    leetcode

--- _1__50/_48/Demo01.java
+++ _1__50/_48/Demo01.java
@@ -2,7 +2,7 @@ package leetcode._1__50._48;
 
 import org.junit.Test;
 
-import java.util.*;
+import java.util.Arrays;
 
 public class Demo01 {
 
@@ -12,22 +12,23 @@ public class Demo01 {
         int[][] image2 = new int[][]{new int[]{5, 1, 9, 11}, new int[]{2, 4, 8, 10}, new int[]{13, 3, 6, 7}, new int[]{15, 14, 12, 16}};
         rotate(image1);
         rotate(image2);
-//        System.out.println(Arrays.deepToString(image1));
+        System.out.println(Arrays.deepToString(image1));
         System.out.println(Arrays.deepToString(image2));
     }
 
     private void rotate(int[][] matrix) {
         int n = matrix.length;
-//        int[][] image = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                int tmp;
-                tmp = matrix[i][j];
-                matrix[i][j] = matrix[n - 1 - j][i];
-                matrix[n - 1 - j][i] = tmp;
+        int middle = n / 2;
+        int tmp;
+        for (int i = 0; i < middle; i++) {
+            for (int j = 0; j < n - 1 - 2 * i; j++) {
+                tmp = matrix[i][i + j];
+                matrix[i][i + j] = matrix[n - 1 - i - j][i];
+                matrix[n - 1 - i - j][i] = matrix[n - 1 - i][n - 1 - i - j];
+                matrix[n - 1 - i][n - 1 - i - j] = matrix[i + j][n - 1 - i];
+                matrix[i + j][n - 1 - i] = tmp;
             }
         }
-//        return image;
     }
 
 }
