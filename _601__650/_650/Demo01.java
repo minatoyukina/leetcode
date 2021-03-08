b3e4cb920d7b99e9e24e19624867520eab7dbf6a
minatoyukina <1096445518@qq.com>
2021-03-08 15:30:40 +0800

    leetcode

--- _601__650/_650/Demo01.java
+++ _601__650/_650/Demo01.java
@@ -7,25 +7,24 @@ public class Demo01 {
 
     @Test
     public void test() {
-        System.out.println(minSteps(3));
+        System.out.println(minSteps(9));
         System.out.println(minSteps(8));
     }
 
     private int minSteps(int n) {
-        if (n < 2) return 2;
-        int[] dp = new int[n];
-        dp[0] = 2;
-        dp[1] = 3;
-        for (int i = 2; i < n; i++) {
-            int x = i / 2;
-            int tmp = 0;
-            for (int j = 0; j < x; j++) {
-                if (j == x / 2) tmp = Math.max(tmp, dp[j] + 2);
-                else tmp = Math.max(tmp, dp[j] + 1);
+        if (n == 1) return 0;
+        int[] dp = new int[n + 1];
+        dp[1] = 0;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = i;
+            for (int j = 2; j <= i / 2; j++) {
+                if (i % j == 0) {
+                    dp[i] = Math.min(dp[i], dp[j] + i / j);
+                }
             }
-            dp[i] = tmp;
         }
-        return dp[n - 1];
+        return dp[n];
     }
 
 
