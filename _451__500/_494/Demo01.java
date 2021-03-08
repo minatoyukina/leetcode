85d001e05518a692618c78aab78a58b07de2a2ba
minatoyukina <1096445518@qq.com>
2021-03-08 14:54:23 +0800

    leetcode

--- _451__500/_494/Demo01.java
+++ _451__500/_494/Demo01.java
@@ -7,27 +7,22 @@ public class Demo01 {
 
     @Test
     public void test() {
-        System.out.println(findTargetSumWays(new int[]{1, 1, 1, 1, 1}, 1));
+        System.out.println(findTargetSumWays(new int[]{0, 0, 0, 0, 0, 0, 0, 0, 1}, 1));
+        System.out.println(findTargetSumWays(new int[]{1, 1, 1, 1, 1}, 3));
         System.out.println(findTargetSumWays(new int[]{1, 1, 1, 1, 1}, 5));
     }
 
     private int findTargetSumWays(int[] nums, int S) {
-        int sum = 0, ans = 0;
-        for (int num : nums) sum += num;
-        if (sum < S) return 0;
-        if (sum == S) return 1;
-        for (int i = 0; i < nums.length; i++) {
-            int x = sum;
-            for (int j = i; j < nums.length; j++) {
-                x = x - 2 * nums[j];
-                if (x < S) break;
-                else if (x == S) {
-                    ans++;
-                    break;
-                }
-            }
+        return back(nums, S, nums.length - 1);
+    }
+
+    private int back(int[] nums, int s, int offset) {
+        if (offset == 0) {
+            if (nums[0] == 0 && s == 0) return 2;
+            return (s == nums[0] || s == -nums[0]) ? 1 : 0;
         }
-        return ans;
+        int num = nums[offset];
+        return back(nums, s - num, offset - 1) + back(nums, s + num, offset - 1);
     }
 
 }
