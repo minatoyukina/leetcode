2c7e13a6a704dfea2b3108b3d9f9f8bf3ba861ee
minatoyukina <1096445518@qq.com>
2021-03-17 16:42:35 +0800

    leetcode

--- _251__300/_264/Demo01.java
+++ _251__300/_264/Demo01.java
@@ -7,12 +7,28 @@ public class Demo01 {
 
     @Test
     public void test() {
-        System.out.println(nthUglyNumber(1));
+        System.out.println(nthUglyNumber(10));
+        System.out.println(nthUglyNumber(1600));
         System.out.println(nthUglyNumber(1690));
     }
 
     private int nthUglyNumber(int n) {
-        return 0;
+        int[] dp = new int[n];
+        dp[0] = 1;
+        int[] arr = {2, 3, 5};
+        for (int i = 1; i < n; i++) {
+            long min = Long.MAX_VALUE;
+            for (int j = 0; j < i; j++) {
+                for (int a : arr) {
+                    if (dp[j] * (long) a > dp[i - 1]) {
+                        min = Math.min(min, dp[j] * (long) a);
+                    }
+
+                }
+            }
+            dp[i] = (int) min;
+        }
+        return dp[n - 1];
     }
 
 }
