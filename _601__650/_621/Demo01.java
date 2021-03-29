5528e3ff6a6f36587c985b1ce1b34b6db8598fe6
minatoyukina <1096445518@qq.com>
2021-03-29 17:50:37 +0800

    leetcode

--- _601__650/_621/Demo01.java
+++ _601__650/_621/Demo01.java
@@ -1,6 +1,5 @@
 package leetcode._601__650._621;
 
-import javafx.util.Pair;
 import org.junit.Test;
 
 import java.util.*;
@@ -15,31 +14,27 @@ public class Demo01 {
     }
 
     private int leastInterval(char[] tasks, int n) {
-        Queue<Pair<Character, Integer>> queue = new PriorityQueue<>((x, y) -> y.getValue() - x.getValue());
+        Queue<Map.Entry<Character, Integer>> queue = new PriorityQueue<>((x, y) -> y.getValue() - x.getValue());
         Map<Character, Integer> map = new HashMap<>();
-        for (char c : tasks) {
-            map.put(c, map.getOrDefault(c, 0) + 1);
-        }
-        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
-            queue.offer(new Pair<>(entry.getKey(), entry.getValue()));
-        }
+        for (char c : tasks) map.put(c, map.getOrDefault(c, 0) + 1);
+        for (Map.Entry<Character, Integer> entry : map.entrySet()) queue.offer(entry);
         int ans = 0;
         while (!queue.isEmpty()) {
-            Stack<Pair<Character, Integer>> stack = new Stack<>();
+            Stack<Map.Entry<Character, Integer>> stack = new Stack<>();
             int count = 0;
             for (int i = 0; i <= n; i++) {
                 if (queue.size() > 0) {
                     count++;
-                    Pair<Character, Integer> poll = queue.poll();
-                    if (poll != null && poll.getValue() > 1)
-                        stack.push(new Pair<>(poll.getKey(), poll.getValue() - 1));
+                    Map.Entry<Character, Integer> poll = queue.poll();
+                    if (poll != null && poll.getValue() > 1) {
+                        poll.setValue(poll.getValue() - 1);
+                        stack.push(poll);
+                    }
                 }
             }
             if (stack.isEmpty()) ans += count;
             else ans += n + 1;
-            while (!stack.isEmpty()) {
-                queue.add(stack.pop());
-            }
+            while (!stack.isEmpty()) queue.add(stack.pop());
         }
         return ans;
     }
