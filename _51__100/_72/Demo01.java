e806c2f09d7c6163e055dc650ce2433010c2f444
minatoyukina <1096445518@qq.com>
2021-05-07 10:34:33 +0800

    leetcode

--- _51__100/_72/Demo01.java
+++ _51__100/_72/Demo01.java
@@ -2,10 +2,6 @@ package leetcode._51__100._72;
 
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class Demo01 {
 
     @Test
@@ -15,10 +11,22 @@ public class Demo01 {
     }
 
     private int minDistance(String word1, String word2) {
-        return 0;
+        int m = word1.length(), n = word2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i * j == 0) {
+                    if (i == 0 && j == 0) dp[i][j] = 0;
+                    else if (i == 0) dp[i][j] = j;
+                    else dp[i][j] = i;
+                } else {
+                    if (word1.charAt(i - 1) == word2.charAt(j - 1)) dp[i][j] = dp[i - 1][j - 1];
+                    else dp[i][j] = Math.min(dp[i - 1][j], Math.min(dp[i - 1][j - 1], dp[i][j - 1])) + 1;
+                }
+            }
+        }
+        return dp[m][n];
     }
 
-    private void dfs(String s1,String s2){
 
-    }
 }
