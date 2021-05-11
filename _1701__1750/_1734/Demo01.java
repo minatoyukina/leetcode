e6f9d6eae3f0ac01b21d5d84c8207f9b09d90523
minatoyukina <1096445518@qq.com>
2021-05-11 13:53:03 +0800

    leetcode

--- _1701__1750/_1734/Demo01.java
+++ _1701__1750/_1734/Demo01.java
@@ -2,12 +2,25 @@ package leetcode._1701__1750._1734;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 public class Demo01 {
 
 
     @Test
     public void test() {
-
+        System.out.println(Arrays.toString(decode(new int[]{6, 5, 4, 6})));
     }
-    
+
+    private int[] decode(int[] encoded) {
+        int n = encoded.length + 1;
+        int tot = 0;
+        int[] ans = new int[n];
+        for (int i = 1; i <= n; i++) tot ^= i;
+        for (int i = 1; i < n - 1; i += 2) tot ^= encoded[i];
+        ans[0] = tot;
+        for (int i = 1; i < n; i++) ans[i] = ans[i - 1] ^ encoded[i - 1];
+        return ans;
+    }
+
 }
