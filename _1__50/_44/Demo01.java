156a0fa03af77cd49bcfab709ffc9c520e358a47
minatoyukina <1096445518@qq.com>
2021-05-14 14:18:44 +0800

    leetcode

--- _1__50/_44/Demo01.java
+++ _1__50/_44/Demo01.java
@@ -7,7 +7,30 @@ public class Demo01 {
 
     @Test
     public void test() {
-
+        System.out.println(isMatch("abcabczzzde", "*abc???de*"));
+        System.out.println(isMatch("", "***********"));
     }
-    
+
+    private boolean isMatch(String s, String p) {
+        dp = new Boolean[s.length() + 1][p.length() + 1];
+        return isMatch(s, p, 0, 0);
+    }
+
+    private Boolean[][] dp;
+
+    private boolean isMatch(String s, String p, int si, int pi) {
+        if (dp[si][pi] != null) return dp[si][pi];
+        if (si >= s.length() && p.substring(pi).replace("*", "").isEmpty()) return dp[si][pi] = true;
+        if (si >= s.length() || pi >= p.length()) return si >= s.length() && pi >= p.length();
+        if (s.charAt(si) == p.charAt(pi) || p.charAt(pi) == '?') return dp[si][pi] = isMatch(s, p, si + 1, pi + 1);
+        if (p.charAt(pi) == '*') {
+            for (int i = si; i <= s.length(); i++) {
+                if (isMatch(s, p, i, pi + 1)) {
+                    return dp[si][pi] = true;
+                }
+            }
+        }
+        return dp[si][pi] = false;
+    }
+
 }
--- _901__950/_925/Demo01.java
+++ _901__950/_925/Demo01.java
@@ -7,7 +7,26 @@ public class Demo01 {
 
     @Test
     public void test() {
-
+        System.out.println(isLongPressedName("saeedi", "ssaaeediixxxiii"));
+        System.out.println(isLongPressedName("leelee", "lleeelee"));
     }
-    
+
+    private boolean isLongPressedName(String name, String typed) {
+        int left = 0, right = 0;
+        int m = name.length(), n = typed.length();
+        if (name.charAt(left) != typed.charAt(right)) return false;
+        while (left < m && right < n) {
+            if (name.charAt(left) == typed.charAt(right)) {
+                left++;
+                right++;
+                if (left == m) while (right < n && typed.charAt(right - 1) == typed.charAt(right)) right++;
+            } else {
+                while (right < n && name.charAt(left) != typed.charAt(right)) {
+                    if (typed.charAt(right - 1) != typed.charAt(right)) return false;
+                    right++;
+                }
+            }
+        }
+        return left == m && right == n;
+    }
 }
