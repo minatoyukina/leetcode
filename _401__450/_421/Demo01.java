e79587860052c51ce0fd349d4c37760aa1ad55b8
minatoyukina <1096445518@qq.com>
2021-05-16 19:56:16 +0800

    leetcode

--- _401__450/_421/Demo01.java
+++ _401__450/_421/Demo01.java
@@ -7,7 +7,17 @@ public class Demo01 {
 
     @Test
     public void test() {
-
+        System.out.println(findMaximumXOR(new int[]{3, 10, 5, 25, 2, 8}));
     }
-    
+
+    private int findMaximumXOR(int[] nums) {
+        int ans = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                ans = Math.max(ans, nums[i] ^ nums[j]);
+            }
+        }
+        return ans;
+    }
+
 }
