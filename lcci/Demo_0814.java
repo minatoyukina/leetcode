339b83c2a835f9d0734b07a281984f75112f5bff
minatoyukina <1096445518@qq.com>
2021-05-18 09:42:59 +0800

    leetcode

--- lcci/Demo_0814.java
+++ lcci/Demo_0814.java
@@ -2,8 +2,6 @@ package leetcode.lcci;
 
 import org.junit.Test;
 
-import java.util.Stack;
-
 /**
  * @author ccq
  * @since 2021/4/14 10:39
@@ -17,10 +15,33 @@ public class Demo_0814 {
     }
 
     private int countEval(String s, int result) {
-        return 0;
+        return dfs(new StringBuilder(s), result);
     }
 
-    private int dfs(Stack<Character> stack, String s, int index) {
-        return 0;
+    private int dfs(StringBuilder s, int result) {
+        if (s.indexOf("|") < 0 && s.indexOf("&") < 0 && s.indexOf("^") < 0) {
+            return Integer.parseInt(s.toString()) == result ? 1 : 0;
+        }
+        int ans = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int x = i;
+            while (x < s.length() && s.charAt(x) >= '0' && s.charAt(x) <= '9') x++;
+            int z = x;
+            int y = z + 1;
+            while (y < s.length() && s.charAt(y) >= '0' && s.charAt(y) <= '9') y++;
+            if (y <= s.length() && x > i && (s.charAt(z) <= '0' || s.charAt(z) >= '9') && y > z + 1) {
+                StringBuilder sb = new StringBuilder(s);
+                int c = compute(Integer.parseInt(sb.substring(i, x)), Integer.parseInt(sb.substring(z + 1, y)), sb.charAt(z));
+                sb.replace(i, y, c + "");
+                ans += dfs(sb, result);
+            }
+        }
+        return ans;
+    }
+
+    private int compute(int x, int y, char c) {
+        if (c == '|') return x | y;
+        if (c == '&') return x & y;
+        else return x ^ y;
     }
 }
