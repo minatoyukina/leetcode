058b6adbd64284e185d7d1a7d51d9055e6f83939
minatoyukina <1096445518@qq.com>
2021-06-09 17:02:35 +0800

    leetcode

--- _851__900/_879/Demo01.java
+++ _851__900/_879/Demo01.java
@@ -2,8 +2,7 @@ package leetcode._851__900._879;
 
 import org.junit.Test;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 
 public class Demo01 {
 
@@ -15,17 +14,22 @@ public class Demo01 {
     }
 
     private int profitableSchemes(int n, int minProfit, int[] group, int[] profit) {
-        dp = new HashMap<>();
+        dp = new int[minProfit + 1][n + 1][group.length + 1];
+        for (int[][] ints : dp) {
+            for (int[] anInt : ints) {
+                Arrays.fill(anInt, -1);
+            }
+        }
         return dfs(n, minProfit, group, profit, 0, 0, 0);
     }
 
-    private Map<Integer, Map<Integer, Map<Integer, Integer>>> dp;
+    private int[][][] dp;
 
     private int dfs(int n, int minProfit, int[] group, int[] profits, int profit, int people, int index) {
+        int key = profit >= minProfit ? minProfit : profit;
         if (people > n) return 0;
         if (index > group.length) return profit >= minProfit ? 1 : 0;
-        if (dp.getOrDefault(profit, new HashMap<>()).getOrDefault(people, new HashMap<>()).get(index) != null)
-            return dp.get(profit).get(people).get(index);
+        if (dp[key][people][index] != -1) return dp[key][people][index];
         int ans = 0;
         for (int i = index; i <= group.length; i++) {
             ans += dfs(n, minProfit, group, profits,
@@ -33,12 +37,7 @@ public class Demo01 {
                     people + (i == group.length ? 0 : group[i]), i + 1);
             ans %= (int) (1e9 + 7);
         }
-        Map<Integer, Map<Integer, Integer>> s1 = dp.getOrDefault(profit, new HashMap<>());
-        Map<Integer, Integer> s2 = s1.getOrDefault(people, new HashMap<>());
-        s2.put(index, ans);
-        s1.put(people, s2);
-        dp.put(profit, s1);
-        return ans;
+        return dp[key][people][index] = ans;
     }
 
 }
