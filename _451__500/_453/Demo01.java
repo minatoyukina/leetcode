a164c5578186ec7de591a5ad79ef75ae503773d1
minatoyukina <1096445518@qq.com>
2021-06-26 14:05:23 +0800

    leetcode

--- _451__500/_453/Demo01.java
+++ _451__500/_453/Demo01.java
@@ -12,30 +12,11 @@ public class Demo01 {
 
 
     private int minMoves(int[] nums) {
-        int n = nums.length;
-        if (n == 1) return 0;
         int min = nums[0];
-        for (int i : nums) {
-            min = Math.min(min, i);
-        }
-        long[] copy = new long[n];
-        for (int i = 0; i < copy.length; i++) {
-            copy[i] = min < 0 ? nums[i] - min : nums[i];
-        }
-        long max = copy[0], mi = copy[0], sum = 0;
-        for (long l : copy) {
-            max = Math.max(max, l);
-            mi = Math.min(mi, l);
-            sum += l;
-        }
-        max = mi == max ? max : max + 1;
-        while (true) {
-            long y = max * n - sum;
-            if (y % (n - 1) == 0) {
-                return (int) (y / (n - 1));
-            }
-            max++;
-        }
+        for (int num : nums) min = Math.min(min, num);
+        int sum = 0;
+        for (int num : nums) sum += num - min;
+        return sum;
     }
 
 }
