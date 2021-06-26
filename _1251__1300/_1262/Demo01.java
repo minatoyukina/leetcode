ba7731f972cdde1e003fbb38f6d002e83bf10eee
minatoyukina <1096445518@qq.com>
2021-06-27 01:10:37 +0800

    leetcode

--- _1251__1300/_1262/Demo01.java
+++ _1251__1300/_1262/Demo01.java
@@ -29,12 +29,12 @@ public class Demo01 {
         d2.sort(Integer::compareTo);
         int a = d1.size();
         int b = d2.size();
-        if (a % 3 == b % 3) return sum;
         int max = 0;
         /*
           两种配对模式，取3n个d1/d2，或者相同数目的d1和d2。
           最后剩下的只可能是1个d1,2个d1,1个d2,2个d2或者全部匹配5种情况。
          */
+        if (a % 3 == b % 3) return Math.max(max, sum);
         if (d1.size() >= 1 && (a - 1) % 3 == b % 3) max = Math.max(max, sum - d1.get(0));
         if (d1.size() >= 2 && (a - 2) % 3 == b % 3) max = Math.max(max, sum - d1.get(0) - d1.get(1));
         if (d2.size() >= 1 && (b - 1) % 3 == a % 3) max = Math.max(max, sum - d2.get(0));
--- _901__950/_909/Demo01.java
+++ _901__950/_909/Demo01.java
@@ -2,12 +2,54 @@ package leetcode._901__950._909;
 
 import org.junit.Test;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+
 public class Demo01 {
 
 
     @Test
     public void test() {
-
+        System.out.println(snakesAndLadders(new int[][]{
+                {-1, -1, -1, -1, -1, -1},
+                {-1, -1, -1, -1, -1, -1},
+                {-1, -1, -1, -1, -1, -1},
+                {-1, 35, -1, -1, 13, -1},
+                {-1, -1, -1, -1, -1, -1},
+                {-1, 15, -1, -1, -1, -1}
+        }));
     }
-    
+
+    private int snakesAndLadders(int[][] board) {
+        int n = board.length;
+        int max = n * n;
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(1);
+        Set<Integer> visited = new HashSet<>();
+        int ans = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            while (size-- > 0) {
+                Integer poll = queue.poll();
+                assert poll != null;
+                if (poll == max) return ans;
+                for (int i = 1; i <= 6; i++) {
+                    int x = poll + i;
+                    if (x > max) continue;
+                    int row = n - 1 - (x - 1) / n;
+                    int col = (n - 1 - row) % 2 == 0 ? (x - 1) % n : (n - 1 - (x - 1) % n);
+                    if (board[row][col] != -1) x = board[row][col];
+                    if (!visited.contains(x)) {
+                        queue.offer(x);
+                        visited.add(x);
+                    }
+                }
+            }
+            ans++;
+        }
+        return -1;
+    }
+
 }
