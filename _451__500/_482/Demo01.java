6b7d57e59a7c37da15f3bd19642811d39e11071d
minatoyukina <1096445518@qq.com>
2021-07-03 21:51:26 +0800

    leetcode

--- _451__500/_482/Demo01.java
+++ _451__500/_482/Demo01.java
@@ -7,7 +7,15 @@ public class Demo01 {
 
     @Test
     public void test() {
-
+        System.out.println(licenseKeyFormatting("5F3Z-2e-9-w", 4));
+        System.out.println(licenseKeyFormatting("2-5g-3-J", 2));
     }
-    
+
+    private String licenseKeyFormatting(String s, int k) {
+        s = s.replace("-", "");
+        StringBuilder sb = new StringBuilder(s);
+        for (int i = sb.length() - k; i > 0; i -= k) sb.insert(i, "-");
+        return sb.toString().toUpperCase();
+    }
+
 }
