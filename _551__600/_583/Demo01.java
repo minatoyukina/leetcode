1ea5e75a002451f14e7e4cbd8c4c51647dfc9a56
minatoyukina <1096445518@qq.com>
2021-07-05 17:43:13 +0800

    leetcode

--- _551__600/_583/Demo01.java
+++ _551__600/_583/Demo01.java
@@ -9,5 +9,16 @@ public class Demo01 {
     public void test() {
 
     }
-    
+
+    private int minDistance(String word1, String word2) {
+        int m = word1.length(), n = word2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+            }
+        }
+        return dp[m][n];
+    }
+
 }
