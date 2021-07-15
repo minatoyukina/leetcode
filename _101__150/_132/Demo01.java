f48438021fdb37e45d21f7502704a5cb4aa14e47
minatoyukina <1096445518@qq.com>
2021-07-15 09:46:40 +0800

    leetcode

--- _101__150/_132/Demo01.java
+++ _101__150/_132/Demo01.java
@@ -17,10 +17,6 @@ public class Demo01 {
     private int minCut(String s) {
         dp = new int[s.length()][s.length()];
         for (int[] ints : dp) Arrays.fill(ints, -1);
-        for (int i = 0; i < s.length(); i++) {
-            partition(s, i, i, 0, s.length() - 1, dp);
-            if (i + 1 < s.length() && s.charAt(i) == s.charAt(i + 1)) partition(s, i, i + 1, 0, s.length() - 1, dp);
-        }
         return dfs(s, 0, s.length() - 1);
     }
 
@@ -28,16 +24,14 @@ public class Demo01 {
 
     private int dfs(String s, int left, int right) {
         if (left >= right) return 0;
-        if (dp[left][right] != -1) {
-            return dp[left][right];
-        }
+        if (dp[left][right] != -1) return dp[left][right];
         int min = Integer.MAX_VALUE;
         for (int i = left; i <= right; i++) {
-            int[] p1 = partition(s, i, i, left, right, dp);
+            int[] p1 = partition(s, i, i, left, right);
             min = Math.min(min, dfs(s, left, p1[0] - 1) + dfs(s, p1[1] + 1, right) +
                     (p1[0] <= left ? 0 : 1) + (p1[1] >= right ? 0 : 1));
             if (i + 1 <= right && s.charAt(i) == s.charAt(i + 1)) {
-                int[] p2 = partition(s, i, i + 1, left, right, dp);
+                int[] p2 = partition(s, i, i + 1, left, right);
                 min = Math.min(min, dfs(s, left, p2[0] - 1) + dfs(s, p2[1] + 1, right) +
                         (p2[0] <= left ? 0 : 1) + (p2[1] >= right ? 0 : 1));
             }
@@ -46,10 +40,9 @@ public class Demo01 {
         return dp[left][right] = min;
     }
 
-    private int[] partition(String s, int l, int r, int left, int right, int[][] dp) {
+    private int[] partition(String s, int l, int r, int left, int right) {
         while (l >= left && r <= right) {
             if (s.charAt(l) == s.charAt(r)) {
-                dp[l][r] = 0;
                 l--;
                 r++;
             } else break;
--- _1801__1850/_1846/Demo01.java
+++ _1801__1850/_1846/Demo01.java
@@ -2,12 +2,24 @@ package leetcode._1801__1850._1846;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 public class Demo01 {
 
 
     @Test
     public void test() {
-
+        System.out.println(maximumElementAfterDecrementingAndRearranging(new int[]{100, 1, 1000}));
     }
-    
+
+    private int maximumElementAfterDecrementingAndRearranging(int[] arr) {
+        Arrays.sort(arr);
+        arr[0] = 1;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > arr[i - 1] + 1)
+                arr[i] = arr[i - 1] + 1;
+        }
+        return arr[arr.length - 1];
+    }
+
 }
