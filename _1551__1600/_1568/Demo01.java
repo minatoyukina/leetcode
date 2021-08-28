58269218a736d3451f06858c2f57534f25180033
minatoyukina <1096445518@qq.com>
2021-08-29 01:22:01 +0800

    leetcode

--- _1551__1600/_1568/Demo01.java
+++ _1551__1600/_1568/Demo01.java
@@ -7,7 +7,20 @@ public class Demo01 {
 
     @Test
     public void test() {
-
+        System.out.println(sumOddLengthSubarrays(new int[]{1, 4, 2, 5, 3}));
     }
-    
+
+    public int sumOddLengthSubarrays(int[] arr) {
+        int[] pre = new int[arr.length + 1];
+        for (int i = 0; i < arr.length; i++) {
+            pre[i + 1] = pre[i] + arr[i];
+        }
+        int sum = 0;
+        for (int step = 1; step <= arr.length; step += 2) {
+            for (int i = 0; i <= arr.length - step; i++) {
+                sum += pre[i + step] - pre[i];
+            }
+        }
+        return sum;
+    }
 }
