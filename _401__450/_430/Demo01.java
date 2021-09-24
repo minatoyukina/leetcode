e6b9bae5131ef7bb8b7bb8359e38cf223c9f7583
minatoyukina <1096445518@qq.com>
2021-09-24 21:22:57 +0800

    leetcode

--- _401__450/_430/Demo01.java
+++ _401__450/_430/Demo01.java
@@ -13,41 +13,33 @@ public class Demo01 {
         Node node = new Node(0);
         node.next = new Node(1);
         node.child = new Node(2);
-        System.out.println(flatten(node));
+        Node flatten = flatten(node);
+        System.out.println(flatten);
     }
 
     public Node flatten(Node head) {
-        List<List<Node>> list = new ArrayList<>();
-        dfs(head, 0, 0, list);
+        List<Integer> list = new ArrayList<>();
+        dfs(head, list);
         Node dummy = new Node();
         Node tmp = dummy;
-        int max = list.stream().mapToInt(List::size).max().orElse(0);
-        for (int i = 0; i < max; i++) {
-            for (List<Node> nodes : list) {
-                if (nodes.size() > i && nodes.get(i) != null) {
-                    Node copy = new Node();
-                    copy.val = nodes.get(i).val;
-                    tmp.next = copy;
-                    tmp = tmp.next;
-                }
-            }
+        for (Integer integer : list) {
+            Node node = new Node();
+            node.val = integer;
+            tmp.next = node;
+            node.prev = tmp;
+            tmp = tmp.next;
         }
+        if (dummy.next != null) dummy.next.prev = null;
         return dummy.next;
     }
 
-    private void dfs(Node head, int level, int pos, List<List<Node>> list) {
+    private void dfs(Node head, List<Integer> list) {
         if (head == null) return;
-        if (list.size() <= level) {
-            List<Node> sub = new ArrayList<>();
-            for (int i = 0; i < pos; i++) sub.add(null);
-            list.add(sub);
-        }
-        list.get(level).add(head);
-        if (head.child != null) dfs(head.child, level + 1, pos, list);
-        if (head.next != null) dfs(head.next, level, pos + 1, list);
+        list.add(head.val);
+        if (head.child != null) dfs(head.child, list);
+        if (head.next != null) dfs(head.next, list);
     }
 
-    @SuppressWarnings("all")
     static class Node {
         public int val;
         public Node prev;
@@ -61,13 +53,6 @@ public class Demo01 {
         public Node() {
         }
 
-        @Override
-        public String toString() {
-            return "Node{" +
-                    "val=" + val +
-                    ", next=" + next +
-                    '}';
-        }
     }
 
 
