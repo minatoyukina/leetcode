d308b7c8895d0dc77e7bacbacb72afb152a6588e
minatoyukina <1096445518@qq.com>
2021-12-01 17:40:22 +0800

    leetcode

--- _1401__1450/_1446/Demo01.java
+++ _1401__1450/_1446/Demo01.java
@@ -7,7 +7,22 @@ public class Demo01 {
 
     @Test
     public void test() {
-
+        System.out.println(maxPower("leetcode"));
+        System.out.println(maxPower("a"));
     }
-    
+
+    public int maxPower(String s) {
+        int max = 1, count = 1;
+        int pos = 0;
+        while (pos < s.length()) {
+            if (pos + 1 < s.length() && s.charAt(pos + 1) == s.charAt(pos)) count++;
+            else {
+                max = Math.max(max, count);
+                count = 1;
+            }
+            pos++;
+        }
+        return max;
+    }
+
 }
