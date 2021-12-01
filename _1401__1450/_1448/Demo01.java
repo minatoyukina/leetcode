513f81d08dfd902540f8edb3e4572945c2426126
minatoyukina <1096445518@qq.com>
2021-12-01 18:10:07 +0800

    leetcode

--- _1401__1450/_1448/Demo01.java
+++ _1401__1450/_1448/Demo01.java
@@ -1,5 +1,7 @@
 package leetcode._1401__1450._1448;
 
+import leetcode._51__100._100.TreeNode;
+import leetcode.util.Common;
 import org.junit.Test;
 
 public class Demo01 {
@@ -7,7 +9,23 @@ public class Demo01 {
 
     @Test
     public void test() {
-
+        System.out.println(goodNodes(Common.arrayToTree(3, 1, 4, 3, null, 1, 5)));
+        System.out.println(goodNodes(Common.arrayToTree(3, 3, null, 4, 2)));
     }
-    
+
+    public int goodNodes(TreeNode root) {
+        dfs(root, root.val);
+        return count;
+    }
+
+    private int count;
+
+    private void dfs(TreeNode node, int pre) {
+        if (node == null) return;
+        if (node.val >= pre) count++;
+        pre = Math.max(node.val, pre);
+        dfs(node.left, pre);
+        dfs(node.right, pre);
+    }
+
 }
