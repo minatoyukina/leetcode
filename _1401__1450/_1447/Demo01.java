e96550ed83cab5aa105d9644eeee15187e38fda4
minatoyukina <1096445518@qq.com>
2021-12-01 17:55:08 +0800

    leetcode

--- _1401__1450/_1447/Demo01.java
+++ _1401__1450/_1447/Demo01.java
@@ -2,12 +2,31 @@ package leetcode._1401__1450._1447;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Demo01 {
 
 
     @Test
     public void test() {
-
+        System.out.println(simplifiedFractions(100));
     }
-    
+
+    public List<String> simplifiedFractions(int n) {
+        List<String> list = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j < i; j++) {
+                if (GCD(j, i) == 1) list.add(j + "/" + i);
+            }
+        }
+        return list;
+    }
+
+    int GCD(int a, int b) {
+        if (b == 0) return a;
+        if (a < b) return GCD(b, a);
+        else return GCD(b, a % b);
+    }
+
 }
