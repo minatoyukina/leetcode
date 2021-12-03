617c93f2e8f7c9fb7c9acf204c7ace7027166f84
minatoyukina <1096445518@qq.com>
2021-12-03 17:52:02 +0800

    leetcode

--- _1001__1050/_1005/Demo01.java
+++ _1001__1050/_1005/Demo01.java
@@ -2,12 +2,23 @@ package leetcode._1001__1050._1005;
 
 import org.junit.Test;
 
+import java.util.Optional;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class Demo01 {
 
 
     @Test
     public void test() {
-
+        System.out.println(largestSumAfterKNegations(new int[]{2, -3, -1, 5, -4}, 2));
     }
-    
+
+    public int largestSumAfterKNegations(int[] nums, int k) {
+        Queue<Integer> queue = new PriorityQueue<>();
+        for (int num : nums) queue.add(num);
+        while (k-- > 0) queue.add(-Optional.ofNullable(queue.poll()).orElse(0));
+        return queue.stream().mapToInt(Integer::intValue).sum();
+    }
+
 }
