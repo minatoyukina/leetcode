4065ad4c0af66978f38aa626b2b7e9dd77f264f1
minatoyukina <1096445518@qq.com>
2022-01-20 10:45:29 +0800

    leetcode

--- _1951__2000/_1974/Demo01.java
+++ _1951__2000/_1974/Demo01.java
@@ -7,7 +7,19 @@ public class Demo01 {
 
     @Test
     public void test() {
-
+        System.out.println(minTimeToType("abc"));
+        System.out.println(minTimeToType("zjpc"));
     }
-    
+
+    public int minTimeToType(String word) {
+        char[] chars = word.toCharArray();
+        char cur = 'a';
+        int ans = 0;
+        for (char c : chars) {
+            int offset = Math.abs(c - cur);
+            ans += (offset > 13 ? (26 - offset) : offset) + 1;
+            cur = c;
+        }
+        return ans;
+    }
 }
