00c02e772782e76d483abc079fe11882bb5a8d28
minatoyukina <1096445518@qq.com>
2022-01-25 10:29:37 +0800

    leetcode

--- _1651__1700/_1689/Demo01.java
+++ _1651__1700/_1689/Demo01.java
@@ -7,7 +7,14 @@ public class Demo01 {
 
     @Test
     public void test() {
-
+        System.out.println(minPartitions("27346209830709182346"));
+        System.out.println(minPartitions("82734"));
     }
-    
+
+    public int minPartitions(String n) {
+        int max = 0;
+        for (int i = 0; i < n.length(); i++) max = Math.max(max, n.charAt(i) - '0');
+        return max;
+    }
+
 }
