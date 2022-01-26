03b3b88240b6629a135a64e575358c6d330408a2
minatoyukina <1096445518@qq.com>
2022-01-26 14:53:58 +0800

    leetcode

--- _1951__2000/_1984/Demo01.java
+++ _1951__2000/_1984/Demo01.java
@@ -2,12 +2,23 @@ package leetcode._1951__2000._1984;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 public class Demo01 {
 
 
     @Test
     public void test() {
-
+        System.out.println(minimumDifference(new int[]{9, 4, 1, 7}, 2));
     }
-    
+
+    public int minimumDifference(int[] nums, int k) {
+        Arrays.sort(nums);
+        int min = Integer.MAX_VALUE;
+        for (int i = k - 1; i < nums.length; i++) {
+            min = Math.min(min, nums[i] - nums[i - k + 1]);
+        }
+        return min;
+    }
+
 }
