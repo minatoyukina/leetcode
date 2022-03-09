b29ba7f9536f0345b3aca8b4236df743ce0f821e
minatoyukina <1096445518@qq.com>
2022-03-09 16:35:04 +0800

    leetcode

--- _1451__1500/_1472/Demo01.java
+++ _1451__1500/_1472/Demo01.java
@@ -2,6 +2,9 @@ package leetcode._1451__1500._1472;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Demo01 {
 
 
@@ -9,5 +12,33 @@ public class Demo01 {
     public void test() {
 
     }
-    
+
+    @SuppressWarnings("all")
+    static class BrowserHistory {
+
+        private List<String> list = new ArrayList<>();
+        private int pos;
+
+        public BrowserHistory(String homepage) {
+            list.add(homepage);
+        }
+
+        public void visit(String url) {
+            list = list.subList(0, pos + 1);
+            list.add(url);
+            pos = list.size() - 1;
+        }
+
+        public String back(int steps) {
+            if (steps > pos) pos = 0;
+            else pos -= steps;
+            return list.get(pos);
+        }
+
+        public String forward(int steps) {
+            if (pos + steps >= list.size()) pos = list.size() - 1;
+            else pos += steps;
+            return list.get(pos);
+        }
+    }
 }
