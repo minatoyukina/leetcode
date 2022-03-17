1a2ef78f9a3053300824578efb9beb99f808cf99
minatoyukina <1096445518@qq.com>
2022-03-17 18:04:36 +0800

    leetcode

--- _1401__1450/_1422/Demo01.java
+++ _1401__1450/_1422/Demo01.java
@@ -7,7 +7,18 @@ public class Demo01 {
 
     @Test
     public void test() {
-
+        System.out.println(maxScore("011101"));
     }
-    
+
+    public int maxScore(String s) {
+        int left = 0, right = (int) s.chars().filter(x -> x == '1').count();
+        int max = 0;
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.charAt(i) == '0') left++;
+            else right--;
+            max = Math.max(max, left + right);
+        }
+        return max;
+    }
+
 }
