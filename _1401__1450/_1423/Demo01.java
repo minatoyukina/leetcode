324437f3bb12a040c60e1333b4b2f0b6977fdbff
minatoyukina <1096445518@qq.com>
2022-03-17 18:18:28 +0800

    leetcode

--- _1401__1450/_1423/Demo01.java
+++ _1401__1450/_1423/Demo01.java
@@ -2,12 +2,25 @@ package leetcode._1401__1450._1423;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 public class Demo01 {
 
 
     @Test
     public void test() {
-
+        System.out.println(maxScore(new int[]{1, 79, 80, 1, 1, 1, 200, 1}, 3));
     }
-    
+
+    public int maxScore(int[] cardPoints, int k) {
+        int left = Arrays.stream(cardPoints).limit(k).sum(), right = 0, max = left;
+        for (int i = k - 1; i >= 0; i--) {
+            left -= cardPoints[i];
+            right += cardPoints[cardPoints.length - (k - i)];
+            max = Math.max(max, left + right);
+        }
+        return max;
+    }
+
+
 }
