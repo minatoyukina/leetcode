8414dc81998f3fde26e03b005d3a81a8d7a34a4c
minatoyukina <1096445518@qq.com>
2022-04-12 11:07:12 +0800

    leetcode

--- _801__850/_806/Demo01.java
+++ _801__850/_806/Demo01.java
@@ -2,12 +2,26 @@ package leetcode._801__850._806;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 public class Demo01 {
 
 
     @Test
     public void test() {
-
+        System.out.println(Arrays.toString(numberOfLines(new int[]{10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10}, "abcdefghijklmnopqrstuvwxyz")));
     }
-    
+
+    private int[] numberOfLines(int[] widths, String s) {
+        int cur = 0, n = 1;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (cur + widths[c - 'a'] > 100) {
+                n++;
+                cur = widths[c - 'a'];
+            } else cur += widths[c - 'a'];
+        }
+        return new int[]{n, cur};
+    }
+
 }
