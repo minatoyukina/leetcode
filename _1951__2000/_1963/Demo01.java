059371b6d8a6ae9967affbd3982ad6b3890da7a3
minatoyukina <1096445518@qq.com>
2023-01-30 17:55:53 +0800

    leetcode

--- _1951__2000/_1963/Demo01.java
+++ _1951__2000/_1963/Demo01.java
@@ -7,7 +7,22 @@ public class Demo01 {
 
     @Test
     public void test() {
-
+        System.out.println(minSwaps("]]][[["));
+        System.out.println(minSwaps("[]"));
     }
-    
+
+    private int minSwaps(String s) {
+        int cnt = 0, ans = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '[') cnt++;
+            else if (cnt > 0) cnt--;
+            else {
+                cnt++;
+                ans++;
+            }
+        }
+        return ans;
+    }
+
 }
