277d5e44d0e07d874d4a1a84b8f395f7d787e2f5
minatoyukina <1096445518@qq.com>
2024-03-08 17:01:52 +0800

    leetcode

--- src/main/java/_1501__1550/_1526/Demo01.java
+++ src/main/java/_1501__1550/_1526/Demo01.java
@@ -2,12 +2,26 @@ package _1501__1550._1526;
 
 import org.junit.Test;
 
+import java.util.Stack;
+
 public class Demo01 {
 
 
     @Test
     public void test() {
-
+        System.out.println(minNumberOperations(new int[]{3, 1, 5, 4, 2}));
+        System.out.println(minNumberOperations(new int[]{1, 2, 3, 2, 1}));
     }
-    
+
+    public int minNumberOperations(int[] target) {
+        int sum = 0;
+        Stack<Integer> stack = new Stack<>();
+        for (int i : target) {
+            if (!stack.isEmpty() && stack.peek() > i) sum += stack.pop() - i;
+            stack.push(i);
+        }
+        if (!stack.isEmpty()) sum += stack.pop();
+        return sum;
+    }
+
 }
