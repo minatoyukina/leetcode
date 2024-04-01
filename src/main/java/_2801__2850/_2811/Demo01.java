aa1e21ababe2719cf1b7faa2d0d6ae5847da7595
minatoyukina <1096445518@qq.com>
2024-04-01 11:10:38 +0800

    leetcode

--- src/main/java/_2801__2850/_2811/Demo01.java
+++ src/main/java/_2801__2850/_2811/Demo01.java
@@ -2,12 +2,20 @@ package _2801__2850._2811;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Demo01 {
 
 
     @Test
     public void test() {
-
+        System.out.println(canSplitArray(Arrays.asList(2, 3, 3, 2, 3), 6));
     }
-    
+
+    public boolean canSplitArray(List<Integer> nums, int m) {
+        for (int i = 0; i < nums.size() - 1; i++) if (nums.get(i) + nums.get(i + 1) >= m) return true;
+        return nums.size() <= 2;
+    }
+
 }
