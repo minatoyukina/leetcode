4743d730e2d7aeaf285ed57d8cd03ef183ad1d0a
minatoyukina <1096445518@qq.com>
2024-04-03 13:42:48 +0800

    leetcode

--- src/main/java/_1451__1500/_1493/Demo01.java
+++ src/main/java/_1451__1500/_1493/Demo01.java
@@ -7,7 +7,24 @@ public class Demo01 {
 
     @Test
     public void test() {
-
+        System.out.println(longestSubarray(new int[]{1, 0, 0, 0}));
+        System.out.println(longestSubarray(new int[]{0, 1, 1, 1, 0, 1, 1, 0, 1}));
     }
-    
+
+    public int longestSubarray(int[] nums) {
+        int max = 0, preLen = 0, preIdx = -1;
+        for (int i = 0; i < nums.length; i++) {
+            int idx = i;
+            while (idx < nums.length && nums[idx] == 1) idx++;
+            if (nums[i] == 1) {
+                if (preIdx == i - 2) max = Math.max(max, preLen + idx - i);
+                preIdx = idx - 1;
+                preLen = idx - i;
+                max = Math.max(idx - i, max);
+                i = idx - 1;
+            }
+        }
+        return max == nums.length ? (max - 1) : max;
+    }
+
 }
