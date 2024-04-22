addcd0aa5dd2d80a9fc142c1f8556492d8ebe909
minatoyukina <1096445518@qq.com>
2024-04-22 18:24:50 +0800

    leetcode

--- src/main/java/_1601__1650/_1616/Demo01.java
+++ src/main/java/_1601__1650/_1616/Demo01.java
@@ -7,7 +7,29 @@ public class Demo01 {
 
     @Test
     public void test() {
-
+        System.out.println(checkPalindromeFormation(
+                "aejbaalflrmkswrydwdkdwdyrwskmrlfqizjezd",
+                "uvebspqckawkhbrtlqwblfwzfptanhiglaabjea"));
+        System.out.println(checkPalindromeFormation("s", "t"));
     }
-    
+
+    public boolean checkPalindromeFormation(String a, String b) {
+        return check(a, b) || check(b, a);
+    }
+
+    public boolean check(String a, String b) {
+        int left = 0, right = a.length() - 1;
+        while (left < right) {
+            if (a.charAt(left) == b.charAt(right)) {
+                left++;
+                right--;
+            } else {
+                String s = a.substring(0, left) + b.substring(left);
+                String t = a.substring(0, right + 1) + b.substring(right + 1);
+                return new StringBuilder(s).reverse().toString().equals(s) || new StringBuilder(t).reverse().toString().equals(t);
+            }
+        }
+        return true;
+    }
+
 }
