26cb25c190c074118dc958dfd2019c99c658623a
minatoyukina <1096445518@qq.com>
2024-06-06 10:43:06 +0800

    leetcode

--- src/main/java/_2901__2950/_2938/Demo01.java
+++ src/main/java/_2901__2950/_2938/Demo01.java
@@ -7,7 +7,18 @@ public class Demo01 {
 
     @Test
     public void test() {
-
+        System.out.println(minimumSteps("111111111100100010"));
     }
-    
+
+    public long minimumSteps(String s) {
+        int left = 0, right = s.length() - 1;
+        long ans = 0;
+        while (left < right) {
+            if (s.charAt(left) == '1' && s.charAt(right) == '0') ans += right-- - left++;
+            if (s.charAt(left) == '0') left++;
+            if (s.charAt(right) == '1') right--;
+        }
+        return ans;
+    }
+
 }
