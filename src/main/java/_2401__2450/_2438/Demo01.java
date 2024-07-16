fe579398e696a9a23dfe02c2285b4d310265cf9b
minatoyukina <1096445518@qq.com>
2024-07-16 16:55:32 +0800

    leetcode

--- src/main/java/_2401__2450/_2438/Demo01.java
+++ src/main/java/_2401__2450/_2438/Demo01.java
@@ -1,13 +1,29 @@
 package _2401__2450._2438;
 
 import org.junit.Test;
+import util.Common;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Demo01 {
 
 
     @Test
     public void test() {
-
+        System.out.println(Arrays.toString(productQueries(15, Common.strToArray("[[0,1],[2,2],[0,3]]"))));
     }
-    
+
+    public int[] productQueries(int n, int[][] queries) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 32; i++) if ((n >> i & 1) == 1) list.add(i);
+        int[] pre = new int[list.size() + 1];
+        for (int i = 0; i < list.size(); i++) pre[i + 1] = pre[i] + list.get(i);
+        int[] ans = new int[queries.length];
+        for (int i = 0; i < queries.length; i++)
+            ans[i] = (int) (Math.pow(2, pre[queries[i][1] + 1] - pre[queries[i][0]]) % ((int) 1e9 + 7));
+        return ans;
+    }
+
 }
