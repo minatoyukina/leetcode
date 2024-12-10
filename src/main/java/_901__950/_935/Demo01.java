f9d1f3be89e26f071b09d41c708d98e49af0d72c
minatoyukina <1096445518@qq.com>
2024-12-10 17:26:45 +0800

    leetcode

--- src/main/java/_901__950/_935/Demo01.java
+++ src/main/java/_901__950/_935/Demo01.java
@@ -2,12 +2,31 @@ package _901__950._935;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 public class Demo01 {
 
 
     @Test
     public void test() {
-
+        System.out.println(knightDialer(3131));
     }
-    
+
+    public int knightDialer(int n) {
+        int[][] arr = new int[][]{{4, 6}, {6, 8}, {7, 9}, {4, 8}, {3, 9, 0}, {}, {1, 7, 0}, {2, 6}, {1, 3}, {2, 4}};
+        int[][] dp = new int[n + 1][10];
+        int mod = (int) 1e9 + 7;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (i == 1) dp[i][j] = 1;
+                else
+                    for (int k : arr[j]) {
+                        dp[i][j] += dp[i - 1][k];
+                        dp[i][j] %= mod;
+                    }
+            }
+        }
+        return Arrays.stream(dp[n]).reduce(0, (x, y) -> (x + y) % mod);
+    }
+
 }
